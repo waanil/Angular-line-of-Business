aa8083ee1c2ea033ae5dd01aaad2289698aad186
anil kumar <anil.svnit@gmail.com>
2016-03-13 23:28:19 +0530

    services related changes

--- Angular Trainning/Angular Business App/app/products/procductDetailController.js
+++ Angular Trainning/Angular Business App/app/products/procductDetailController.js
@@ -1,10 +1,13 @@
 ﻿(function () {
     "use strict";
-    angular.module("productManagement").controller("productDetailController", ["$scope", "product", productDetailController]);
-    function productDetailController($scope, product) {
+    angular.module("productManagement").controller("productDetailController", ["$scope", "product","productService", productDetailController]);
+    function productDetailController($scope, product,productService) {
 
         $scope.product = product;
         $scope.title = "Product Deatil :" + $scope.product.productName;
+        
+        $scope.marginPercent =
+            productService.calculateMarginPercent($scope.product.price, $scope.product.cost);
         if ($scope.product.tag) {
             $scope.product.tagList = productDetailController.tag.toString();
         }
